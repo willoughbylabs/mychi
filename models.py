c8169b9ca1115ef6b7fdebb109c24c0f7b494da9
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2020-12-30 08:48:14 -0600

    Add model for train line table

--- models.py
+++ models.py
@@ -8,3 +8,12 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Line(db.Model):
+    """ Table to store lines and their identifiers. """
+
+    __tablename__ = "lines"
+
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)

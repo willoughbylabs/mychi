c7827a710c9dd70ad8c2149d4e9c92c354b4995f
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2020-12-31 10:32:15 -0600

    Add db model for train stops

--- models.py
+++ models.py
@@ -22,4 +22,24 @@ class Line(db.Model):
         """ Represent model object as line ID and name. """
 
         line = self
-        return f"<Line ID: {line.id} - {line.name}"
+        return f"<Line ID: {line.id} - {line.name}>"
+
+
+class Stop(db.Model):
+    """ Table to store train stops. """
+
+    __tablename__ = "stops"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    stop_id = db.Column(db.Integer, nullable=False)
+    line_id = db.Column(db.String, db.ForeignKey("lines.id"), nullable=False)
+    stop_name = db.Column(db.String, nullable=False)
+    station_name = db.Column(db.String, nullable=False)
+
+    line = db.relationship("Line", backref="stops")
+
+    def __repr__(self):
+        """ Represent stop with line_id and stop name. """
+
+        stop = self
+        return f"<Stop: Line ID: {stop.line_id}, Station: {stop.stop_name}>"

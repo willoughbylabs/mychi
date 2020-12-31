e033ac6f3d443a3647775242e466d81f68605cd5
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2020-12-31 10:33:15 -0600

    Generate list of train stops from external API

---
+++ seed_helpers.py
@@ -0,0 +1,41 @@
+import requests
+from models import Line, Stop
+from keys import socrata_key
+
+#  ##### Train Lines #####
+
+
+def generate_train_lines():
+    """ Generates hardcoded list of train lines. """
+    red = Line(id="red", name="Red Line")
+    blue = Line(id="blue", name="Blue Line")
+    brown = Line(id="brn", name="Brown Line")
+    green = Line(id="g", name="Green Line")
+    orange = Line(id="o", name="Orange Line")
+    purple = Line(id="p", name="Purple Line")
+    pink = Line(id="pnk", name="Pink Line")
+    yellow = Line(id="y", name="Yellow Line")
+    # purple_exp = Line(id="pexp", name="Purple Line Express")
+
+    lines_list = [red, blue, brown, green, orange, pink, purple, yellow]
+    return lines_list
+
+
+def get_stations():
+    """ Retrieves stations from List of 'L' stops API. """
+
+    stations = []
+    url = "https://data.cityofchicago.org/resource/8pix-ypme.json"
+    line_ids = ["red", "blue", "brn", "g", "o", "p", "pnk", "y"]
+    for line in line_ids:
+        params = {"$$app_token": socrata_key, line: "true"}
+        response = requests.get(url, params=params).json()
+        for station in response:
+            new_stop = Stop(
+                stop_id=station["stop_id"],
+                line_id=line,
+                stop_name=station["stop_name"],
+                station_name=station["station_name"],
+            )
+            stations.append(new_stop)
+    return stations

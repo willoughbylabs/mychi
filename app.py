e3f707e6f1643bea7412988c0618c73166ee9448
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2020-12-30 22:43:54 -0600

    Add route for transit prediction

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db
+from models import connect_db, Line
+from forms import TransitTrainForm
 
 
 app = Flask(__name__)
@@ -21,3 +22,21 @@ def display_home_page():
     """ Display welcome page with synopsis of website. """
 
     return render_template("base.html")
+
+
+@app.route("/transit")
+def display_transit_dashboard():
+    """ Display form and dashboard for monitoring arrival predictions for train stops. """
+
+    form = TransitTrainForm()
+    lines = Line.query.all()
+    line_choices = [(line.id, line.name) for line in lines]
+    form.line.choices = line_choices
+    return render_template("transit.html", form=form)
+
+
+@app.route("/api/lines")
+def get_train_lines():
+    """ Get list of train lines from database. """
+
+    return redirect(url_for("display_transit_dashboard"))

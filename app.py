1b35703405c57d8c20fe19ecc1c5fbabe65b8966
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2020-12-31 21:31:57 -0600

    Add route to obtain JSON prediction from CTA API

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from operator import itemgetter
 from models import db, connect_db, Line, Stop
 from forms import TransitTrainForm
+from app_helpers import get_prediction
 
 
 app = Flask(__name__)
@@ -32,14 +33,14 @@ def display_transit_dashboard():
     form = TransitTrainForm()
     lines = Line.query.all()
     line_choices = [(line.id, line.name) for line in lines]
-    line_choices.insert(0, (None, "Choose..."))
+    line_choices.insert(0, ("", "Choose..."))
     form.line.choices = line_choices
 
     return render_template("transit.html", form=form)
 
 
 @app.route("/api/<line_id>/stations")
-def get_train_lines(line_id):
+def get_stations(line_id):
     """ Get list of stations for a particular line from database. """
 
     stations_db = Stop.generate_stations(line_id)
@@ -58,3 +59,15 @@ def get_stops():
     stops = sorted(stops_db, key=itemgetter("stop_name"))
 
     return jsonify(stops=stops)
+
+
+@app.route("/transit/prediction")
+def get_arrival_prediction():
+    """ Get arrival prediction for a provided stop ID. """
+
+    stop_id = request.args["stopID"]
+    line_id = request.args["line"]
+    response = get_prediction(stop_id, line_id)
+    print("****************")
+    print(response)
+    return f"Getting prediction for {stop_id} on {line_id}"

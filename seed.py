05317e1cfbf853b06a35fac4e126eab5dcc47573
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2020-12-31 18:12:14 -0600

    Add fix for spelling error in API data

--- seed.py
+++ seed.py
@@ -1,6 +1,6 @@
 from app import app
-from models import db
-from seed_helpers import generate_train_lines, get_stations
+from models import db, Stop
+from seed_helpers import generate_train_lines, generate_stations_list
 
 # ##### Setup #####
 db.drop_all()
@@ -14,6 +14,12 @@ db.session.commit()
 
 # ##### Stations #####
 
-stations_list = get_stations()
+stations_list = generate_stations_list()
 db.session.add_all(stations_list)
+db.session.commit()
+
+# ##### Fix Dan Ryan Spelling Error from API #####
+
+stop = Stop.query.filter(Stop.stop_id == 30088).one_or_none()
+stop.station_name = "95th/Dan Ryan"
 db.session.commit()

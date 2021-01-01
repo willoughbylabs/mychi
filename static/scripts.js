f403cb6d2019ca0b28b282a3fc46cc4999f94339
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2020-12-31 19:43:50 -0600

    Add handlers helper functions for form dropdowns

--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,66 @@
 const $line = $("#line");
+const $station = $("#station");
+const $direction = $("#direction");
 
-$line.on("change", getStops);
+/* EVENT HANDLERS */
+$line.on("change", changeLine);
+$station.on("change", changeStation);
 
-function getStops() {
-    console.log("Getting stops");
+/* DROPDOWN FUNCTIONALITY */
+
+// Get and display stations for selected line.
+async function changeLine() {
+    const stations = await getStations();
+    displayStations(stations);
+    changeStation();
+}
+
+// Get JSON of stations in database for selected line.
+async function getStations() {
+    const lineID = $line.val();
+    const response = await axios.get(`http://127.0.0.1:5000/api/${lineID}/stations`);
+    return response;
+}
+
+// Display stations in dropdown for selected line.
+function displayStations(stations) {
+    $station.empty();
+    const stationsArr = stations.data.stations;
+    const selected = "<option selected>Choose...</option>";
+    $station.append(selected);
+    stationsArr.forEach(station => {
+        const option = `<option value="${station.station_name}">${station.station_name}</option>`;
+        $station.append(option);
+    });
+}
+
+// Get and display stops (direction) for a selected line and station.
+async function changeStation() {
+    const stops = await getStops();
+    displayStops(stops);
+}
+
+// Get JSON of stops (direction) for a selected line and station.
+async function getStops() {
+    const lineID = $line.val();
+    const stationName = $station.val();
+    const response = await axios.get("/api/stops", {
+        params: {
+            line: lineID,
+            station: stationName
+        }
+    });
+    return response;
+}
+
+// Display stops (direction) in dropdown for selected line and station.
+function displayStops(stops) {
+    $direction.empty();
+    const stopsArr = stops.data.stops;
+    const selected = "<option selected>Choose...</option>";
+    $direction.append(selected);
+    stopsArr.forEach(stop => {
+        const option = `<option value="${stop.stop_id}">${stop.stop_name}</option>`;
+        $direction.append(option);
+    })
 }

acb8118930651052002003d8ef2c4e9056da0bd4
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-02 10:16:18 -0600

    Fix line identifier to match prediction API

--- app_helpers.py
+++ app_helpers.py
@@ -5,6 +5,10 @@ from keys import train_tracker_key
 def get_prediction(stop_id, line_id):
     """ Retrieve arrival prediction for a """
 
+    if line_id == "o":
+        line_id = "org"
+    if line_id == "pnk":
+        line_id = "pink"
     url = "https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?"
     response = requests.get(
         f"{url}key={train_tracker_key}&stpid={stop_id}&rt={line_id}&outputType=JSON"

a1661f80fec44b477fada64fc3f203b4a6f10525
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-05 16:59:30 -0600

    Add teardown and attempt duplicate line test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,15 @@
 import os, sys
+import unittest
 from app import app
 from unittest import TestCase
 from models import db, Line, Stop
+from sqlalchemy.exc import IntegrityError
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-app.config["ENV"] == "testing"
+
 db.drop_all()
 db.create_all()
 
@@ -23,8 +25,26 @@ class LineModelTestCase(TestCase):
         db.session.commit()
         self.line = line
 
+    def tearDown(self):
+        db.session.rollback()
+
     def test_add_line(self):
         """ Verify a new line can be added to database. """
 
         self.assertEqual(self.line.id, "blue")
         self.assertEqual(len(Line.query.all()), 1)
+
+        new_line = Line(id="red", name="Red Line")
+        db.session.add(new_line)
+        db.session.commit()
+
+        self.assertEqual(len(Line.query.all()), 2)
+
+    @unittest.skip("Returns FlushError instead of IntegrityError")
+    def test_duplicate_line(self):
+        """ Verify integrity error if duplicate line is added. """
+
+        new_line = Line(id="blue", name="Blue Line")
+        db.session.add(new_line)
+
+        self.assertRaises(IntegrityError, db.session.commit)

6240c7335a54b10daf9790a56faf41c25f6b3fa0
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-05 08:15:14 -0600

    Add field input required validation messages

--- forms.py
+++ forms.py
@@ -6,6 +6,13 @@ from wtforms.validators import InputRequired
 class TransitTrainForm(FlaskForm):
     """ Form for selecting a line, stop, and direction to recieve arrival predictions for. """
 
-    line = SelectField("'L' Line", validators=[InputRequired()])
-    station = SelectField("Station", validators=[InputRequired()])
-    direction = SelectField("Direction", validators=[InputRequired()])
+    line = SelectField(
+        "'L' Line", validators=[InputRequired(message="Please select a train line.")]
+    )
+    station = SelectField(
+        "Station", validators=[InputRequired(message="Please select a station.")]
+    )
+    direction = SelectField(
+        "Direction",
+        validators=[InputRequired(message="Please select a direction of travel.")],
+    )

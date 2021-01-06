d251b437647b0f6276ae213f53bc22b988021481
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-06 11:31:32 -0600

    Add tests for static landing pages

---
+++ tests/test_landing_views.py
@@ -0,0 +1,32 @@
+import os, sys
+from app import app
+from unittest import TestCase
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+
+class LandingViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_page(self):
+        """ Test that home page displays correctly. """
+
+        with self.client as client:
+            response = client.get("/")
+            html = response.get_data(as_text=True)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("<h3>A Concise Data Dashboard</h3>", html)
+
+    def test_about_page(self):
+        """ Test that about page displays correctly. """
+
+        with self.client as client:
+            response = client.get("/about")
+            html = response.get_data(as_text=True)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("<h1>About myChi</h1>", html)

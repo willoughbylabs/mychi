f2ba13f0cf95029e4a82b97fdcd825e0f77c50cc
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-06 12:01:20 -0600

    Add tests for transit views

---
+++ tests/test_transit_views.py
@@ -0,0 +1,53 @@
+import os, sys
+from app import app
+from unittest import TestCase
+from models import db, Line, Stop
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+db.drop_all()
+db.create_all()
+
+
+class TransitPredictionViewTestCase(TestCase):
+    """ Test for transit prediction views. """
+
+    def setUp(self):
+        """ Add a new line and stop for each test. """
+
+        self.client = app.test_client()
+        line = Line(id="red", name="Red Line")
+        db.session.add(line)
+        db.session.commit()
+        self.line = line
+
+        stop = Stop(
+            stop_id="30191",
+            line_id="red",
+            stop_name="69th (Howard-bound)",
+            station_name="69th",
+        )
+        db.session.add(stop)
+        db.session.commit()
+        self.stop = stop
+
+    def test_transit_without_session(self):
+        """ Test transit view without saved stops in session. """
+
+        with self.client as client:
+            response = client.get("/transit")
+            html = response.get_data(as_text=True)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(
+                '<button type="submit" class="btn btn-secondary mb-3">Get Predictions</button>',
+                html,
+            )
+            self.assertIn('<option value="">Choose...</option>', html)
+
+    # def test_transit_with_session(self):
+    #     """ Test transit view with a saved stop in session. """
+
+    #     data = {'line': self.line., 'stop': '30274'}

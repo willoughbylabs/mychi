d79edcbe1bcd4413c5105ba916c6c00610ff6ff6
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-08 01:19:00 -0600

    Add 404 error handling for API spelling fix

--- seed.py
+++ seed.py
@@ -3,6 +3,7 @@ from models import db, Stop
 from seed_helpers import generate_train_lines, generate_stations_list
 
 # ##### Setup #####
+# ? How best to implement the suggestion to retry logic that falls back on the previous version, rather than wiping the db each time?
 db.drop_all()
 db.create_all()
 
@@ -20,6 +21,8 @@ db.session.commit()
 
 # ##### Fix Dan Ryan Spelling Error from API #####
 
-stop = Stop.query.filter(Stop.stop_id == 30088).one_or_none()
+stop = Stop.query.filter(Stop.stop_id == 30088).first_or_404(
+    description="Unable to fix Dan Ryan spelling error from API data."
+)
 stop.station_name = "95th/Dan Ryan"
 db.session.commit()

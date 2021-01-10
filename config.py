e81a09ef5c03bff21a86a81d89f32bac6c923618
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-09 22:16:30 -0600

    Configure database URI from environment variable

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "postgresql:///myChi"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql:///myChi")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SECRET_KEY = os.environ.get("SECRET_KEY", "no-secret-key-found")

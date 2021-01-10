3ff7a726c1a1e65d07ec7bac5408b08382c671ed
Willoughby Labs <30395608+willoughbylabs@users.noreply.github.com>
2021-01-09 19:25:32 -0600

    Finalize for version 0.1

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from app_helpers import (
 
 # Configure application.
 app = Flask(__name__)
-app.config.from_object("config.TestingConfig")
+app.config.from_object("config.Config")
 
 # Connect to database.
 connect_db(app)
